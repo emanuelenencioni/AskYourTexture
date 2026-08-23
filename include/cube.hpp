3a9456d077e040af47c112f662d16612632e7c3b
Emanuele Nencioni <emanencio@hotmail.com>
2026-08-23 11:24:30 +0200

    Face struct moved out class

--- include/cube.hpp
+++ include/cube.hpp
@@ -4,34 +4,36 @@
 #include <vector>
 #include <glad/glad.h>
 
+struct Face {
+    float color[3];
+    float normal[3];
+    int   tris[2][3];
+};
+
 class Cube {
-public:
-    Cube();
-    ~Cube();
+    public:
+        Cube();
+        ~Cube();
 
-    void setupGL();
-    void draw() const;
+        void setupGL();
+        void draw() const;
 
-    const float* vertexData() const { return vertices_.data(); }
-    std::size_t vertexCount() const { return vertices_.size() / 9; }
-    
+        const float* vertexData() const { return vertices_.data(); }
+        std::size_t vertexCount() const { return vertices_.size() / 9; }
+        
+        std::vector<float> getCorners() const;
 
-private:
-    struct Face {
-        float color[3];
-        float normal[3];
-        int   tris[2][3];
-    };
+    private:
 
-    float corners_[8][3];
-    Face  faces_[6];
-    std::vector<float> vertices_;
+        void buildVertices();
+        void tris_from_exact_position();
 
-    unsigned int VBO_;
-    unsigned int VAO_;
+        Face  faces_[6];
+        std::vector<float> vertices_;
+        float corners_[8][3];
 
-    void buildVertices();
-    void tris_from_exact_position();
+        unsigned int VBO_;
+        unsigned int VAO_;
 };
 
 #endif
